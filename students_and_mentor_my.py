532d79bef13b24e2abf6b03ceb7375a0661f9b17
Anton Gusev <antgusev@yandex.ru>
2023-06-04 12:25:38 +0500

    Task_2

--- students_and_mentor_my.py
+++ students_and_mentor_my.py
@@ -6,13 +6,27 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+
+    def rate_lecture(self, lecturer, course, grade):
+        if isinstance(lecturer, Student) and course in self.courses_attached and course in lecturer.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
         
+class Lecturer(Mentor):
+    def lecturer_grades(self):
+        self.grades = {}
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -21,10 +35,6 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-        
-class Lecturer(Mentor):
-
-class Reviewer(Mentor):
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
