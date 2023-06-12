f9fe9a8904c88e47ad0e8314bc6dc903956508d5
Anton Gusev <antgusev@yandex.ru>
2023-06-12 16:38:43 +0500

    add task 3

--- students_and_mentor_my.py
+++ students_and_mentor_my.py
@@ -8,7 +8,7 @@ class Student:
         self.grades = {}
 
     def rate_lecture(self, lecturer, course, grade):
-        if isinstance(lecturer, Student) and course in self.courses_attached and course in lecturer.courses_in_progress:
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in self.courses_in_progress:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -16,15 +16,32 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        return f'Имя: {self.name}\n'
+    f'Фамилия: {self.surname}\n'
+    f'Средняя оценка за домашние задания: {self.ever_grade()}\n'
+    f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
+    f'Завершенные курсы: {", ".join(self.finished_courses)}'
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
+    
+    def __str__(self):
+        return f'Имя: {self.name}\n'f'Фамилия: {self.surname}'
         
 class Lecturer(Mentor):
-    def lecturer_grades(self):
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
         self.grades = {}
+
+    def __str__(self):
+        return f'Имя: {self.name}\n'f'Фамилия: {self.surname}\n'f'Средняя оценка за лекции: {self.rate_lecture()}'
+
 
 class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
