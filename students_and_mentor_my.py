3628dfc06de7e8de8c2afedb416a5d7dce3407af
Anton Gusev <antgusev@yandex.ru>
2023-06-15 22:03:21 +0500

    add task 4

--- students_and_mentor_my.py
+++ students_and_mentor_my.py
@@ -7,8 +7,8 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
-    def rate_lecture(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in self.courses_in_progress:
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.courses_attached and course in lecturer.courses_attached:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -17,11 +17,11 @@ class Student:
             return 'Ошибка'
 
     def __str__(self):
-        return f'Имя: {self.name}\n'
-    f'Фамилия: {self.surname}\n'
-    f'Средняя оценка за домашние задания: {self.ever_grade()}\n'
-    f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
-    f'Завершенные курсы: {", ".join(self.finished_courses)}'
+        return f'Имя: {self.name}\n'\
+            f'Фамилия: {self.surname}\n'\
+                f'Средняя оценка за домашние задания: {self.ever_grade()}\n'\
+                    f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'\
+                        f'Завершенные курсы: {", ".join(self.finished_courses)}'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -29,9 +29,6 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
     
-    def __str__(self):
-        return f'Имя: {self.name}\n'f'Фамилия: {self.surname}'
-        
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         self.name = name
@@ -41,6 +38,8 @@ class Lecturer(Mentor):
 
     def __str__(self):
         return f'Имя: {self.name}\n'f'Фамилия: {self.surname}\n'f'Средняя оценка за лекции: {self.rate_lecture()}'
+    
+    #def __lt__(self, other):
 
 
 class Reviewer(Mentor):
@@ -52,7 +51,10 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
+
+    def __str__(self):
+        return f'Имя: {self.name}\n' f'Фамилия: {self.surname}'
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
@@ -63,4 +65,27 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
  
-print(best_student.grades)
+#print(best_student.grades)
+
+elena_guseva = Student('Elena', 'Guseva', 'female')
+elena_guseva.courses_in_progress += ['Python']
+elena_guseva.courses_in_progress += ['C++']
+ksenya_guseva = Student('Ksenya', 'Guseva', 'female')
+ksenya_guseva.courses_in_progress += ['Python']
+ksenya_guseva.courses_in_progress += ['C++']
+
+
+oleg_bulygin = Lecturer('Oleg', 'Bulygin')
+oleg_bulygin.courses_attached += ['Python']
+oleg_bulygin.courses_attached += ['C++']
+elena_nikitina = Lecturer('Elena', 'Nikitina')
+elena_nikitina.courses_attached += ['Python']
+elena_nikitina.courses_attached += ['C++']
+
+aleksander_bardin = Reviewer('Aleksander', 'Bardin')
+aleksander_bardin.courses_attached += ['Python']
+aleksander_bardin.courses_attached += ['C++']
+denis_rudakov = Reviewer('Denis', 'Rudakov')
+denis_rudakov.courses_attached += ['Python']
+denis_rudakov.courses_attached += ['C++']
+
