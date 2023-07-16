6b7d7baf1c41aa86e3a6cb5baa95c39df1711627
Anton Gusev <antgusev@yandex.ru>
2023-07-16 15:33:28 +0500

    task 4

--- students_and_mentor_my.py
+++ students_and_mentor_my.py
@@ -19,7 +19,7 @@ class Student:
     def __str__(self):
         return f'Имя: {self.name}\n'\
             f'Фамилия: {self.surname}\n'\
-                f'Средняя оценка за домашние задания: {self.ever_grade()}\n'\
+                f'Средняя оценка за домашние задания: {self._rate_hw()}\n'\
                     f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'\
                         f'Завершенные курсы: {", ".join(self.finished_courses)}'
 
@@ -30,6 +30,14 @@ class Student:
             sum_ += sum(course)
             count += len(course)
         return round(sum_ / count, 2)
+    
+        #средняя оценка за домашние задания по всем студентам в рамках конкретного курса
+    def get_avg_stud_grade(students, course):
+        sum_ = 0
+        for student in students:
+            sum_ += sum(student.grades[course]) / len(student.grades[course])
+
+        return sum_ / len(students)
 
 
 class Mentor:
@@ -46,9 +54,9 @@ class Lecturer(Mentor):
         self.courses_attached = []
         self.grades = {}
 
-#    def __init__(self, name, su  rname):
-#       super().__init__(name, surname)
-#       self.grades = {}
+    # def __init__(self, name, surname):
+    #    super().__init__(name, surname)
+    #    self.grades = {}
 
     def __str__(self):
         return f'Имя: {self.name}\n'f'Фамилия: {self.surname}\n'f'Средняя оценка за лекции: {self._get_average_grade()}'
@@ -73,6 +81,7 @@ class Lecturer(Mentor):
             count += len(course)
         return round(sum_ / count, 2)
     
+    #средняя оценка за лекции всех лекторов в рамках курса
     def get_avg_lect_grade(lecturers, course):
         sum_ = 0
         for lecturer in lecturers:
@@ -81,9 +90,10 @@ class Lecturer(Mentor):
         return sum_ / len(lecturers)
 
 
-
-
 class Reviewer(Mentor):
+    def __str__(self):
+        return f'Имя: {self.name}\n' f'Фамилия: {self.surname}'
+    
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in self.courses_in_progress:
             if course in student.grades:
@@ -93,43 +103,54 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'
 
-    def __str__(self):
-        return f'Имя: {self.name}\n' f'Фамилия: {self.surname}'
 
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+# best_student.courses_in_progress += ['Git']
+# best_student.rate_lecturer(oleg_bulygin, 'Python', 5)
+# best_student.rate_lecturer(oleg_bulygin, 'Python', 4)
+# best_student.rate_lecturer(oleg_bulygin, 'Git', 5)
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+# print(get_avg_lect_grade([oleg_bulygin], 'Python'))
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
  
 
 #Студенты
 
 elena_guseva = Student('Elena', 'Guseva', 'female')
 elena_guseva.courses_in_progress += ['Python']
-elena_guseva.courses_in_progress += ['C++']
-ksenya_guseva = Student('Ksenya', 'Guseva', 'female')
-ksenya_guseva.courses_in_progress += ['Python']
-ksenya_guseva.courses_in_progress += ['C++']
+elena_guseva.courses_in_progress += ['Git']
+elena_guseva.rate_lecturer(lecturer2, 'Python', 5)
+elena_guseva.rate_lecturer(lecturer2, 'Python', 4)
+elena_guseva.rate_lecturer(lecturer2, 'Git', 5)
 
-#print (ksenya_guseva)
+
+ksenya_guseva = Student('Ksenya', 'Guseva', 'female')
+ksenya_guseva.courses_in_progress += ['Python'] 
+ksenya_guseva.courses_in_progress += ['C++']
+ksenya_guseva.rate_lecturer(lecturer1, 'Python', 5)
+ksenya_guseva.rate_lecturer(lecturer1, 'Python', 4)
+ksenya_guseva.rate_lecturer(lecturer1, 'Git', 5)
+
+
+# print (ksenya_guseva)
 
 
 #Лекторы
 
-oleg_bulygin = Lecturer('Oleg', 'Bulygin')
-oleg_bulygin.courses_attached += ['Python']
-oleg_bulygin.courses_attached += ['C++']
-elena_nikitina = Lecturer('Elena', 'Nikitina')
-elena_nikitina.courses_attached += ['Python']
-elena_nikitina.courses_attached += ['C++']
+lecturer1 = Lecturer('Oleg', 'Bulygin')
+lecturer1.courses_attached += ['Python']
+lecturer1.courses_attached += ['Git']
 
-#print(oleg_bulygin)
+lecturer2 = Lecturer('Elena', 'Nikitina')
+lecturer2.courses_attached += ['Python']
+lecturer2.courses_attached += ['Git']
 
 
 #Проверяющие
@@ -137,6 +158,7 @@ elena_nikitina.courses_attached += ['C++']
 aleksander_bardin = Reviewer('Aleksander', 'Bardin')
 aleksander_bardin.courses_attached += ['Python']
 aleksander_bardin.courses_attached += ['C++']
+
 denis_rudakov = Reviewer('Denis', 'Rudakov')
 denis_rudakov.courses_attached += ['Python']
 denis_rudakov.courses_attached += ['C++']
